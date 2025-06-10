aff8a9262081eb564a69040cb3d35475ab2d60e3
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-06-10 10:26:18 -0400

    fix: edit button prefills education inputs

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,45 +5,35 @@ let nextId = 0;
 
 function Education() {
   const [education, setEducation] = useState([]);
-  const [editable, setEditable] = useState("true");
-  const [currentSchoolId, setCurrentSchoolId] = useState("");
+  const [editData, setEditData] = useState({editable: true, currentSchoolId: ''});
 
-  function handleAddSchool(e) {
+  function handleSubmit(e) {
     e.preventDefault();
     const newSchoolName = document.getElementById("school-name").value;
     const newYears = document.getElementById("years").value;
     const newArea = document.getElementById("area").value;
+    if (editData.currentSchoolId === '') {
     setEducation([
-      ...education,
-      { id: nextId++, name: newSchoolName, years: newYears, area: newArea },
-    ]);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setEditable("false");
-  }
-
-  function handleSchoolEdit(e) {
-    e.preventDefault();
-    const schoolId = e.target.id;
-    const school = education.filter((school) => school.id === schoolId);
-    const schoolName = school.name;
-    const schoolYears = school.years;
-    const schoolArea = school.area;
-    const newEducation = education.map((school) => {
-      if (school.id !== schoolId) {
-        return school;
+          ...education,
+          { id: nextId++, name: newSchoolName, years: newYears, area: newArea }
+        ]);
       } else {
-        return {
-          ...school,
-          name: schoolName,
-          years: schoolYears,
-          area: schoolArea,
-        };
+        setEducation(education.map(school => {
+          if (school.id === editData.currentSchoolId) {
+            return {...school, name: newSchoolName, years: newYears, area: newArea}
+          } else {
+            return school;
+          }
+        }))
       }
-    });
-    setEducation(newEducation);
+    
+    setEditData({editable: false, currentSchoolId: ''});
+  }
+
+  function handleEditButton(e) {
+    e.preventDefault();
+    const editingSchoolId = e.target.dataset.schoolId;
+    setEditData({editable: true, currentSchoolId: editingSchoolId});
   }
 
   function handleSchoolDelete(e) {
@@ -52,33 +42,58 @@ function Education() {
     setEducation(education.filter(school => school.id !== deleteId));
   }
 
-  if (editable === "true") {
+  if (editData.editable === true) {
+    let schoolName = '';
+    let schoolArea = '';
+    let schoolYears = '';
+    if (editData.currentSchoolId !== '') {
+      const schoolToEdit = education.filter(school => school.id === parseInt(editData.currentSchoolId));
+      console.log(schoolToEdit);
+      schoolName = schoolToEdit[0].name;
+      schoolYears = schoolToEdit[0].years;
+      schoolArea = schoolToEdit[0].area;
+    }
     return (
       <section>
-        <form onSubmit={handleAddSchool}>
-          <CustomInput id="school-name" label="School Name" text="" />
-          <CustomInput id="years" label="Years Attended:" text="" />
-          <CustomInput id="area" label="Area of Study:" text="" />
-          <button>Add School</button>
+        <form onSubmit={handleSubmit}>
+          <CustomInput id="school-name" label="School Name" text={schoolName} />
+          <CustomInput id="years" label="Years Attended:" text={schoolArea} />
+          <CustomInput id="area" label="Area of Study:" text={schoolYears} />
+          <button>Submit</button>
         </form>
-        {education.map((school) => {
-          return (
-            <div class="school">
-              <h2>{school.name}</h2>
-              <h4>{school.years}</h4>
-              <p>{school.area}</p>
-              <button id={school.id} onClick={handleSchoolEdit}>
-                Edit
-              </button>
-              <button id={school.id} onClick={handleSchoolDelete}>
-                Delete
-              </button>
-            </div>
-          );
-        })}
-        <button onClick={handleSubmit}>Done</button>
+
+        {education.length > 0 ? 
+          education.map((school) => {
+            return (
+              <div key={school.id} className="school">
+                <h2>{school.name}</h2>
+                <h4>{school.years}</h4>
+                <p>{school.area}</p>
+              </div>
+            );
+          }) : null}
       </section>
-    );
+    )
+  } else {
+    return (
+      <section>
+        {education.map((school) => {
+            return (
+              <div key={school.id} className="school">
+                <h2>{school.name}</h2>
+                <h4>{school.years}</h4>
+                <p>{school.area}</p>
+                <button data-school-id={school.id} onClick={handleEditButton}>
+                  Edit
+                </button>
+                <button  data-school-id={school.id} onClick={handleSchoolDelete}>
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+      </section>
+    )
   }
 }
 
